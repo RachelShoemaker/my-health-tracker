9993710baa3548f089e67f1693eae4004c31185e
Rachel Shoemaker <rshoema5@asu.edu>
2025-04-14 15:19:38 -0500

    Added get-all-wts, get-one-wt, edit-wt, and delete-wt routes in the Index.js

--- server/db.js
+++ server/db.js
@@ -1,5 +1,10 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    
-})
+    user: "postgres",
+    password: "HelloWorld",
+    database: "health_data",
+    port: 5432
+});
+
+module.exports = pool;
--- server/index.js
+++ server/index.js
@@ -1,10 +1,68 @@
 const express = require("express");
 const app = express(); // Takes express lib and runs it
 const cors = require("cors");
+const pool = require("./db");
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); // req.body
 
+// Routes
+
+// Create a food or weight entry.
+app.post("/weights", async(req, res) => {
+    // await, lets func complete before the tales is ready
+    try {
+        const { measurement_date, weight } = req.body;
+        const newWeight = await pool.query("INSERT INTO weight_entries (measurement_date, weight) VALUES($1, $2) RETURNING *", [measurement_date, weight]);
+
+        res.json(newWeight.rows[0]);
+    } catch (e) {
+        console.error(e.message);
+    }
+})
+// Get all entries to list on the bottom half of a dashboard.
+app.get("/weights", async(req, res) => {
+    try {
+        const allWeights = await pool.query("SELECT * FROM weight_entries");
+        res.json(allWeights.rows);
+    }
+catch (e) {
+    console.error(e.message);
+}});
+
+// Get one entry.
+app.get("/weights/:measurement_date", async (req, res) => {
+    try {
+        const { measurement_date } = req.params;
+        const weight = await pool.query("SELECT * FROM weight_entries WHERE measurement_date = $1", [measurement_date]);
+        res.json(weight.rows[0]);
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+// Update an entry.
+app.put("/weights/:measurement_date", async (req, res) => {
+    try {
+        const { measurement_date } = req.params;
+        const { weight } = req.body;
+        const updateWeight = await pool.query("UPDATE weight_entries SET weight = $1 WHERE measurement_Date = $2", [weight, measurement_date]);
+        res.json("Weight updated");
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+// Delete an entry.
+app.delete("/weights/:measurement_date", async (req, res) => {
+    try {
+        const { measurement_date } = req.params;
+        const deleteWeight = await pool.query("DELETE FROM weight_entries WHERE measurement_date = $1", [measurement_date]);
+        res.json("Weight deleted");
+    } catch (error) {
+        console.error(error.message);
+    }
+})
 
 app.listen(5000, () => {
     console.log("started server on port 5000")
