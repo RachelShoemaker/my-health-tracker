b37d1baabf9ff6f11e3ad7207c263efc47b380a4
Rachel Shoemaker <rshoema5@asu.edu>
2025-04-15 17:11:20 -0500

    Adjusted schema and created sql script to generate 100 sample data.

--- server/database.sql
+++ server/database.sql
@@ -1,18 +1,16 @@
 CREATE DATABASE health_data;
 
-CREATE TABLE calorie_entries (
-  entry_id SERIAL PRIMARY KEY,
-  entry_date DATE NOT NULL,
-  meal_type VARCHAR(20),   -- e.g., Breakfast, Lunch, Dinner, Snack
-  calories INTEGER NOT NULL,
-  note TEXT,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
 CREATE TABLE weight_entries (
   entry_id SERIAL PRIMARY KEY,
   measurement_date DATE NOT NULL,
   weight INTEGER NOT NULL,
-  note TEXT,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+-- Inserts 100 days worth of entries starting from today's date to 99 days in the past. 
+-- Weights can be between 100 and 150.
+-- 
+INSERT INTO weight_entries (measurement_date, weight)
+SELECT gs::date AS measurement_date,
+       (FLOOR(100 + random() * 50))::int AS weight
+FROM generate_series(CURRENT_DATE - INTERVAL '1 day', CURRENT_DATE - INTERVAL '100 days', '-1 day') AS gs;
